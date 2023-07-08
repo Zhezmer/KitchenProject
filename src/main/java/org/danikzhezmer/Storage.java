3447edf8fc48e5f43ccf6610d22c3694ce6584b4
Danik Zhezmer <danik_zhezmer@mail.ru>
2023-07-08 17:09:07 +0300

    fixed int to AtomicInteger and create method

--- src/main/java/org/danikzhezmer/Storage.java
+++ src/main/java/org/danikzhezmer/Storage.java
@@ -2,21 +2,19 @@ package org.danikzhezmer;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Storage {
     private HashMap<Integer, String> map = new HashMap<>();
-    private int id = 0;
+    private AtomicInteger id = new AtomicInteger(0);
 
-    public Storage(HashMap<Integer, String> map, int id) {
+    public Storage(HashMap<Integer, String> map, AtomicInteger id) {
         this.map = map;
         this.id = id;
     }
 
     public Storage() {
 
-    }
-    public int idIncGet(int id){
-        return this.id = id + 1;
     }
 
     public String get(int id) {
@@ -28,7 +26,8 @@ public class Storage {
     }
 
     public void create(String value) {
-        map.putIfAbsent(idIncGet(id), value);
+        id.incrementAndGet();
+        map.putIfAbsent(id.get(), value);
     }
 
     public void update(int id, String value) {
