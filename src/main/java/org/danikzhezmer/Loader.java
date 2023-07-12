9da6b6fd6b4d3a65167fd3eb92388868a36a9ff9
Danik Zhezmer <danik_zhezmer@mail.ru>
2023-07-12 17:28:42 +0300

    save and load from file

--- src/main/java/org/danikzhezmer/Loader.java
+++ src/main/java/org/danikzhezmer/Loader.java
@@ -1,27 +1,46 @@
 package org.danikzhezmer;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collections;
+import org.danikzhezmer.model.Book;
+
+import java.io.*;
 import java.util.Map;
 
 public class Loader {
-    //todo в последнюю очередь сразу с буком реализовать
     private static final String FILE_NAME = "map.properties";
+    String path = System.getProperty("user.home") + File.separator + FILE_NAME;
 
-    public void saveToFile(Map<Integer, String> map) {
-
-        //todo realize it
+    public void saveToFile(Map<Integer, Book> map) {
+        try {
+            FileOutputStream fos = new FileOutputStream(getFileOrCreateIfNotExists());
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(map);
+            oos.flush();
+            oos.close();
+            fos.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Saving is failed" + e.getMessage());
+        }
     }
 
 
-    public Map<Integer, String> loadFromFile() {
-        //todo realize it
-        return Collections.EMPTY_MAP;
+    public Map<Integer, Book> loadFromFile() {
+        Map<Integer, Book> mapInFile;
+        try {
+            FileInputStream fis = new FileInputStream(getFileOrCreateIfNotExists());
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            mapInFile = (Map<Integer, Book>) ois.readObject();
+            ois.close();
+            fis.close();
+            for (Map.Entry<Integer, Book> m : mapInFile.entrySet()) {
+                System.out.println(m.getKey() + " : " + m.getValue());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Loading is failed " + e.getMessage());
+        }
+        return mapInFile;
     }
 
     private File getFileOrCreateIfNotExists() throws IOException {
-        String path = System.getProperty("user.home") + File.separator + FILE_NAME;
         File file = new File(path);
         if (!file.exists()) {
             file.createNewFile();
@@ -30,3 +49,4 @@ public class Loader {
     }
 
 }
+
